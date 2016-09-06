666d79fce572e4ed707c9500d2940ada14972ee5
aleeex <akang2@gmail.com>
2016-09-06 11:24:39 -0600

    created ui component

--- retain/src/app/containers/main.ts
+++ retain/src/app/containers/main.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+import { AppBar } from '../ui/app-bar';
+
+
 @Component({
   selector: 'main-container',
+  directives: [
+    AppBar
+  ],
   templateUrl: './main-container.component.html',
   styleUrls: ['./main-container.component.css']
 })
---
+++ retain/src/app/ui/app-bar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bar',
+  templateUrl: './app-bar.component.html',
+  styleUrls: ['./app-bar.component.css']
+})
+export class AppBar {
+
+}
