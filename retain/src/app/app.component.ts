c557853d9c05cbb6374912a06bc0f9506278619a
aleeex <akang2@gmail.com>
2016-09-06 10:53:40 -0600

    created another container component, this time for footer. i'm understanding how container components work

--- retain/src/app/app.component.ts
+++ retain/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 import { MainContainerComponent } from './containers/main';
+import { FooterContainerComponent } from './containers/footer-container';
 
 @Component({
   selector: 'app-root',
   directives: [
-    MainContainerComponent
+    MainContainerComponent,
+    FooterContainerComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
---
+++ retain/src/app/containers/footer-container.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-container',
+  templateUrl: './footer-container.component.html',
+  styleUrls: ['./footer-container.component.css']
+})
+export class FooterContainerComponent {
+  
+}
