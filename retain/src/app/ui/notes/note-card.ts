55bb5448627df8ed8fb8fe09a4fa968defd8413f
aleeex <akang2@gmail.com>
2016-09-07 09:36:13 -0600

    adding explanation for [note] property (essentially an html attribute) for note-card.ts

--- retain/src/app/ui/notes/note-card.ts
+++ retain/src/app/ui/notes/note-card.ts
@@ -4,6 +4,10 @@ import {Component} from '@angular/core';
 
 @Component ({
   selector: 'note-card',
+  // here you're using the built-in 'inputs' property
+  // to create a custom attribute called 'note'.
+  // you'll use this on your notes-container using
+  // this syntax [note]
   inputs: ['note'],
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.css']
