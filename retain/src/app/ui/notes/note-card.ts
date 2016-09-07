7a8fa9d31c38dd3c1564b7a7037acd2ea72aa139
aleeex <akang2@gmail.com>
2016-09-07 13:08:37 -0600

    added the property level decorate @Input

--- retain/src/app/ui/notes/note-card.ts
+++ retain/src/app/ui/notes/note-card.ts
@@ -4,16 +4,11 @@ import {Component, Input} from '@angular/core';
 
 @Component ({
   selector: 'note-card',
-  // here you're using the built-in 'inputs' property
-  // to create a custom attribute called 'note'.
-  // you'll use this on your notes-container using
-  // this syntax [note]
-  inputs: ['noteprop'],
   templateUrl: './note-card.component.html',
   styleUrls: ['./note-card.component.css']
 })
 
 export class NoteCard {
-  noteprop = {
+  @Input('noteprop') noteprop = {
   }
 };
